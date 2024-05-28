4a6aadc871728523dc362896ff31fcd27cdadbb5
Mikita Saladukha <malkomus@gmail.com>
2024-05-28 15:55:41 +0300

    rol security with db without encryption

--- src/main/java/com/mikkita/spring/security/configuration/MyConfig.java
+++ src/main/java/com/mikkita/spring/security/configuration/MyConfig.java
@@ -1,5 +1,6 @@
 package com.mikkita.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,9 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 
 @Configuration
@@ -20,5 +24,23 @@ public class MyConfig {
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
     }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+//            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&amp;serverTimezone=UTC");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            //throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+
+        return dataSource;
+    }
+
 
 }
